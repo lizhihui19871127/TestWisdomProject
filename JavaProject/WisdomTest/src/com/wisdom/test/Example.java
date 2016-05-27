7ba3a5f4e6b96fe1439106922c5fedfc2df7ee46
lizhihui <420259856@qq.com>
2016-05-27 22:51:38 +0800

    no message

---
+++ JavaProject/WisdomTest/src/com/wisdom/test/Example.java
@@ -0,0 +1,13 @@
+package com.wisdom.test;
+
+public class Example {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("success");
+	}
+
+}
